97643ef4df631b861c7bef770827430b58087f5f
Vitus Balazs <vitusbalazs01@yahoo.com>
2022-04-03 17:01:29 +0300

    maybe last js commit

--- Lab2/main.js
+++ Lab2/main.js
@@ -2,22 +2,54 @@ let showSubmit = false;
 
 function testEmail() {
     let email = document.getElementById("email").value;
-    if (!(email.endsWith("@gmail.com") || email.endsWith("@yahoo.com"))) {
-        alert("You can only use @gmail.com or @yahoo.com e-mail addresses!");
-        document.getElementById("email").value = "";
+    if (email.endsWith("@gmail.com") || email.endsWith("@yahoo.com")) {
+        return true;
+    } else {
+        return false;
     }
 }
 
 function testWebsite() {
     let website = document.getElementById("favweb").value;
-    //alert(website);
-    if (!(website.match(/https:\/\/(?:www\.)*[A-Za-z0-9]*\.[A-Za-z0-9]*\..+/))) {
-        alert("Your favourite website must contain one domain and one subdomain!");
-        document.getElementById("favweb").value = "";
+    if (website.match(/https:\/\/(?:www\.)*[A-Za-z0-9]*\.[A-Za-z0-9]*\..+/)) {
+        return true;
+    } else {
+        return false;
     }
 }
 
+// last modified
 document.getElementById("modifyDate").innerHTML = document.lastModified;
+
+document.getElementById("submit").disabled = true;
+function validateForm() {
+    let myEmailTest = testEmail();
+    let myWebTest = testWebsite();
+    let validEmail = document.forms.formid.email.validity.valid;
+    let validWeb = document.forms.formid.favweb.validity.valid;
+    let validOperator = (document.getElementById("operator").value.length != 0);
+    let validNumber = (parseInt(document.getElementById("number").value) >= 5 && parseInt(document.getElementById("number").value) <= 10)
+
+    // ha a validalas oke, akkor leveszi a submitrol a disabled dolgot, majd return true, maskepp false
+    if (myEmailTest && myWebTest && validEmail && validWeb && validOperator && validNumber) {
+        document.getElementById("submit").disabled = false;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// ha a submitre nyomok es megsem valid a form, akkor hibauzenet (azert kellett kulon szedjem mert onChange mindenre lefut a validalas a submit gomb elohozasa miatt)
+function validateFormSubmit() {
+    if (!validateForm()) {
+        alert("Hiba validalas kozben!");
+        return false;
+    }
+    else
+        return true;
+}
+
+// maga a JS jatek resz
 
 let tomb1 = [], tomb2 = [], results = [];
 let firstColumnPair, activeNow, column2;
@@ -28,10 +60,12 @@ let showResults;
 let num;
 
 function JSProblem() {
+    // kikerme a numot es az operatort
     document.getElementById("jsresults").style.visibility = "hidden";
     num = document.getElementById("number").value;
     operator = document.getElementById("operator").value;
 
+    // minden jatek elejen visszaallitom az ertekeket
     tomb1 = [];
     tomb2 = [];
     results = [];
@@ -41,6 +75,7 @@ function JSProblem() {
     firstColumnPair = new Array(parseInt(num)).fill(-1);
     column2 = new Array(parseInt(num)).fill(0);
 
+    // operator switch statement, majd kigeneralom az ertekeket es az eredmenyeket ugy hogy 100-on belul legyenek
     for (i=0;i<num;i++)
     {
         let szam1, szam2;
@@ -69,20 +104,22 @@ function JSProblem() {
             case "/":
                 szam1 = Math.floor(Math.random() * 100) + 1;
                 szam2 = Math.floor(Math.random() * 100) + 1;
-                results.push(Math.round(szam1 / szam2));
+                results.push(szam1 / szam2);
                 break;
         }
-        // console.log(szam1 + " " + operator + " " + szam2);
+        
+        // pusholom a szamokat es az eredmenyt tombbe
         tomb1.push(szam1);
         tomb2.push(szam2);
     }
 
-    // randomize array's items
+    // randomizalom az eredmeny tombom elemeinek sorrendjet
     for (let i = results.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [results[i], results[j]] = [results[j], results[i]];
     }
 
+    // kirajzolja elsore a canvast
     redrawCanvas();
 }
 
@@ -94,21 +131,27 @@ function redrawCanvas() {
     
     for (i=0;i<num;i++)
     {
+        // kiirja az elso oszlop elemeit
         ctx.fillText(tomb1[i] + " " + operator + " " + tomb2[i], 20, (i + 1) * 20);
+        // negyzet szin aszerint hogy melyik elem az aktiv eppen
         if (i == activeNow)
             ctx.strokeStyle = "#FF0000";
         else
             ctx.strokeStyle = "#000000";
+        // negyzet kirajzolasa
         ctx.strokeRect(15, i*20 + 7, 70, 15);
+        // majd ugyanez csak a masodik oszlopra es ott mindig fekete negyzet
         ctx.fillText(results[i], 120, (i + 1) * 20);
         ctx.strokeStyle = "#000000";
         ctx.strokeRect(115, i*20 + 7, 30, 15);
 
+        // vonalak kirajzolasa (-1 jelzi hogy egy elemtol nem kell meg vonalat huzni sehova)
         if (firstColumnPair[i] != -1) {
             ctx.beginPath();
             ctx.moveTo(15 + 70, i*20 + 7 + 7.5);
             ctx.lineTo(115, firstColumnPair[i]*20 + 7 + 7.5);
             let ii = firstColumnPair[i];
+            // ha az eredmenyt kell kiertekelni es ugy huzni a vonalat, akkor helyes eseten zold, maskent piros, ha nem kell kiertekelni akkor fekete
             if (showResults && getSum(i) == results[ii])
                 ctx.strokeStyle = "#00FF00";
             else if (showResults)
@@ -122,12 +165,9 @@ function redrawCanvas() {
 
 window.onload = () => {
     const canvas = document.getElementById('myCanvas');
-    // const num = document.getElementById("number").value
-    //let operator = document.getElementById("operator").value
 
     // rendelünk egy figyelőt a canvashez
     canvas.addEventListener('click', event => {
-        //console.log("click");
         const x = event.pageX - canvas.offsetLeft;
         const y = event.pageY - canvas.offsetTop;
         
@@ -138,10 +178,9 @@ window.onload = () => {
                 // elso oszlop
                 if (y >= i*20 + 7 && y <= i*20 + 7 + 15)
                 {
-                    // console.log(1 + " " + (i+1));
+                    // ha epp nincs kivalasztva az elso oszlopbol elem, es olyanra kattintottam akinek meg nincs parja jobbrol akkor o lesz a kivalasztott, piros keret, redraw canvas
                     if (!firstColumnActive && firstColumnPair[i] == -1)
                     {
-                        // console.log("not active first column item");
                         firstColumnActive = true;
                         activeNow = i;
                         redrawCanvas();
@@ -153,7 +192,7 @@ window.onload = () => {
                 // masodik oszlop
                 if (y >= i*20 + 7 && y <= i*20 + 7 + 15)
                 {
-                    // console.log(2 + " " + (i+1));
+                    // ha van epp kivalasztva az elso oszlopbol elem es a jelenlegi masodik oszlopu elem akire kattintottam meg nincs parja, akkor osszekotom oket es redraw
                     if (firstColumnActive && column2[i] == 0) {
                         firstColumnActive = false;
                         firstColumnPair[activeNow] = i;
@@ -161,6 +200,7 @@ window.onload = () => {
                         activeNow = -1;
                         ++numOfPairs;
                         redrawCanvas();
+                        // ha mindenkinek van parja akkor megjelenik a kiertekeles gomb
                         if (numOfPairs == num) {
                             document.getElementById("jsresults").style.visibility = "visible";
                         }
@@ -184,49 +224,22 @@ function getSum(i) {
             sum = tomb1[i] * tomb2[i];
             break;
         case "/":
-            sum = Math.round(tomb1[i] / tomb2[i]);
+            sum = tomb1[i] / tomb2[i];
             break;
     }
     return sum;
 }
 
 function jsresults() {
+    // ha ranyomnak a kiertekeles gombra, akkor ez a fuggveny fog lefutni, kiszamolja hany ok van, hany nem ok, ezt printeli konzolra, majd beallitja hogy szeretnem latni az eredmenyeket, es ujrarajzolja a canvast
     let ok = 0;
     for (i=0;i<numOfPairs;i++) {
         let ii = firstColumnPair[i];
         let sum = getSum(i);
         if (sum == results[ii])
             ++ok;
-        //console.log(sum + " " + results[ii]);
     }
     showResults = true;
     redrawCanvas();
     console.log("Helyes: " + ok + ", helytelen: " + (numOfPairs-ok));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-    /*let c = document.getElementById("myCanvas");
-    let ctx = c.getContext("2d");
-    ctx.font = "15px Arial";
-    ctx.clearRect(0, 0, c.width, c.height);
-    
-    for (i=0;i<num;i++)
-    {
-        //ctx.strokeRect(20,i*20,60,15);
-        ctx.fillText(tomb1[i] + " " + operator + " " + tomb2[i], 20, (i + 1) * 20);
-        ctx.strokeStyle = "#000000";
-        ctx.strokeRect(15, i*20 + 7, 70, 15);
-        ctx.fillText(results[i], 120, (i + 1) * 20);
-        ctx.strokeStyle = "#000000";
-        ctx.strokeRect(115, i*20 + 7, 30, 15);
-    }*/
